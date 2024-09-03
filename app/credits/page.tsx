f321d8f27c6aafac9f2ce29238d3ec58848ef6d9
Andy Nguyen <aervnu@gmail.com>
2024-09-03 20:52:20 +0700

    JS is weird

--- app/credits/page.tsx
+++ app/credits/page.tsx
@@ -6,9 +6,9 @@ const page = () => {
         <h1><strong>Special thanks</strong></h1>
         <br/>
         <p>
-           This site wouldn't have been made possible without support from these people:
+           This site wouldn&apos;t have been made possible without support from these people:
            <br /><br />
-           - <a href="https://floatingmilkshake.com/">Milkshake</a> for the base frontend (well, most of the HTML code plus some parts of the CSS that is. I've successfully moved the page over to Vercel, but it's more or less still an indirect adaptation.)
+           - <a href="https://floatingmilkshake.com/">Milkshake</a> for the base frontend (well, most of the HTML code plus some parts of the CSS that is. I&apos;ve successfully moved the page over to Vercel, but it&apos;s more or less still an indirect adaptation.)
            <br /><br />
            - <a href="https://erisa.uk/">Erisa</a> for her advices on DNS stuff. (fun fact: this page used to be hosted on Cloudflare Pages before being moved to Vercel because GitHub somehow takes an eternity to issue an SSL certificate.)
            <br /><br />
