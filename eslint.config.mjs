5be3e8c0bacf4a19b86dff1771343ab29f6b7c74
Andy Nguyen <aervnu@gmail.com>
2025-05-30 11:56:00 +0700

    disabled tsx warnings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import { off } from "process";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -25,7 +26,7 @@ export default [
     rules: {
       "react/prop-types": "off",
       "react/react-in-jsx-scope": "off",
-      "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+      "react/jsx-filename-extension": off,
     },
   },
 ];
