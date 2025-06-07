4bf8cb822261ec7b9e1698d01f55cef3ae3e3c95
Andy Nguyen <icorei783@gmail.com>
2025-06-08 03:26:16 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,35 +7,35 @@ const Page = () => {
           <strong>Hi, it&apos;s your favorite radio station on air.</strong>
         </h1>
         <br />
-        <p>2007. I break things and play too much Exponential Idle.</p>
+        <p>2007 born cosmic disequilibrium survivor.</p>
         <br />
+        <p>Aroace, I think.</p>
         <h1>
-          <strong>What I like</strong>
+          <strong>What I (happen to) like</strong>
         </h1>
         <p>
           <br />
-          - Regular expressions and low level computing. Fun stuff, trust me.
+          - Regex. Why not.
           <br /><br />
           - Guns. Cannons. Missiles. Fighters too of course. Basically everything used in air and anti air warfare.
           <br />
           Speaking of. I should read more about ballistics.
           <br /><br />
-          - Photography. Well sorta. I haven&apos;t touched my camera for a good while.
+          - Photo...? Sorta.
         </p>
         <br />
         <h1>
           <strong>
-            Plans for the future (if one of these comes true someone on this
-            planet will win 500000 dollars)
+            Plans for the future
           </strong>
         </h1>
         <p>
           <br />
-          - New languages? DE and JP maybe. B2 and N2 respectively I guess.
+          - New language. Probably German or Japanese.
           <br /><br />
-          - Computer networking. I&apos;ll make sure to get a CCNA before I die.
+          - Networking degree. CCNA or die.
           <br /><br />
-          - JS and C++. I don&apos;t know, the hype looks big enough.
+          - JS and C++...maybe?
         </p>
         <br />
         <h1>
@@ -48,9 +48,8 @@ const Page = () => {
             <a href="https://discordapp.com/users/717255311060238387">
               @aervnu
             </a>
-            ; I also talk in the{" "}
-            <a href="discord.gg/microsoft">Microsoft Community server</a> a lot,
-            be sure to check it out.
+            ; also check out{" "}
+            <a href="discord.gg/microsoft">Microsoft Community server</a> if you like
           </li>
           <li>
             Signal:{" "}
