36c365243ee81cd479413d85f56a8aad5ca896c4
Andy Nguyen <icorei783@gmail.com>
2026-02-07 18:25:42 +0700

    what the hell is happening

--- app/devices/page.tsx
+++ app/devices/page.tsx
@@ -89,12 +89,11 @@ const page = async () => {
   try {
     const devicesResponse = await fetch("https://devicejson.aervnu.moe/", {
       cache: "no-store",
-      headers: {
-        "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-        "Content-Type": "application/json",
-      },
-    });
+        headers: {
+          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+          "Content-Type": "application/json"
+  },
+});
     const devices: devicesJsonResponse = await devicesResponse.json();
 
     return (
